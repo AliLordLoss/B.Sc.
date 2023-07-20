31e9806448fc5a905b3ea9076de23a3f5967a9eb
AliLordLoss <a.a.ghanati@gmail.com>
2023-07-21 00:23:18 +0330

    - fixed file name for subscription that caused an error (added .txt to the end of file)

--- MQTT/mqtt.py
+++ MQTT/mqtt.py
@@ -14,12 +14,8 @@ def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
 
 def on_message(client, userdata, msg):
-    # try: 
     with open(msg.topic + ".txt", 'a') as f:
         write_in_file(f, msg.payload.decode('ascii'))
-    # except FileNotFoundError:
-    #     with open(msg.topic, 'w') as f:
-    #         write_in_file(f, msg.payload.decode('ascii'))
 
 message_broker = os.environ['MQTT_BROKER']
 
