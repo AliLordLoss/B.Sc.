349175a948928e329877662e1268b569ae8b439c
AliLordLoss <a.a.ghanati@gmail.com>
2023-08-14 13:07:45 +0330

    - added option to see received messages in MQTT

--- MQTT/main.py
+++ MQTT/main.py
@@ -27,7 +27,7 @@ client.connect(message_broker, 1883, 60)
 client.loop_start()
 
 while True:
-    option = input("Please pick one of the options below:\n  1. subscribe to a topic\n  2. publish a message to a topic\n  3. exit\n")
+    option = input("Please pick one of the options below:\n  1. subscribe to a topic\n  2. publish a message to a topic\n  4. show recieved messages of a topic\n  0. exit\n")
     if option == "1":
         topic = input("Please enter the topic you want to subscribe to:\n")
         client.subscribe(topic)
@@ -36,6 +36,10 @@ while True:
         payload = input("Please enter your message:\n")
         client.publish(topic, payload=payload, qos=0, retain=False)
     elif option == "3":
+        topic = input("Please enter the topic you want to see:\n")
+        with open(topic + ".txt", 'r') as f:
+            print(f.readlines())
+    elif option == "0":
         break
     else:
         print("Invalid option, please try again!")
