4251c7b121f9c6d8f32a571b49d9469fb0abc159
AliLordLoss <a.a.ghanati@gmail.com>
2023-08-18 19:21:21 +0330

    - added code for CAN protocol communication

---
+++ CAN/main.py
@@ -0,0 +1,44 @@
+import can
+import threading
+from datetime import datetime
+
+
+def write_in_file(file, msg):
+        print(f"new message received on {datetime.now()}", file=file)
+        print(f"payload: {msg}\n", file=file)
+
+
+def rcv(bus):
+    while True:
+        data = bus.recv()
+        data = ''.join(map(chr, data))
+        topic, msg = data.split(',')
+        with open(topic + ".txt", 'a') as f:
+            write_in_file(f, msg)
+
+
+bustype = 'socketcan'
+channel = 'can0'
+bus = can.interface.Bus(channel=channel, bustype=bustype, bitrate=125000)
+
+threading.Thread(target=rcv, args=(bus, )).start()
+
+while True:
+    cmd = input('Please select one of the options below:\n  1. Send a message\n  2. See received messages of a topic\n  0. exit\n')
+    if cmd == '1':
+        topic = input('Please enter the topic you want to send message on:\n')
+        msg = input('Please enter the message you want to send:\n')
+        data = topic + "," + msg
+        data = list(map(ord, data))
+        bus.send(can.Message(arbitration_id=0, data=data, is_extended_id=False))
+    elif cmd == '2':
+        topic = input('Please enter the topic you want to see:\n')
+        try:
+            with open(topic + ".txt", 'r') as f:
+                print(*f.readlines(), sep='')
+        except FileNotFoundError:
+            print('There are no messages on this topic yet :(')
+    elif cmd == '0':
+        break
+    else:
+        print('Invalid option!')
